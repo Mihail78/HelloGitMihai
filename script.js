be5284f827d7bb6b87ab3d314cc92e39e738ba9a
Mihai Temciuc <temciucm@gmail.com>
2020-10-14 10:04:58 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/*console.log(4/0);
+console.log('string'*9);
+let something;
+console.log(something);
+*/
+/*let persone = {
+	name: "Mihai",
+	age: 25,
+	isMarried: false
+};*/
+
+/*console.log(persone.name);
+console.log(persone.age);
+console.log(persone.isMarried);
+console.log(persone["name"]);*/
+
+/*var arr = ['orange.png', 'plum.jpg', 'lily.bmp'];
+
+console.log(arr[0]);*/
+
+/*let answer = confirm("Esti de acord?");
+console.log(answer);*/
+
+/*let answer = prompt("Aveti 18 ani?", "DA");
+console.log(typeof(null));
+*/
+
+/*let incr = 10,
+	 decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" == 2);
+console.log("2" === 2);*/
+
+let isChecked = false,
+	 isClosed = false;
+
+console.log(isChecked || isClosed);
